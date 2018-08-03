01f359f2ae0ab9d070e86b663bf8eeabef00af43
Rafael Said <rafasaid@gmail.com>
2018-08-03 15:54:34 -0300

    Aula 09

---
+++ aula09.py
@@ -0,0 +1,12 @@
+frase = 'Curso em Vídeo Python'
+print(frase.upper())
+print(frase.lower())
+print(len(frase))
+divisao = frase.split()
+print(divisao)
+print(divisao[2][1])
+print(frase.replace('Python', 'Android'))
+print(frase.capitalize())
+print(frase.find('o'))
+print('o' in frase)
+print('#'.join(frase))
